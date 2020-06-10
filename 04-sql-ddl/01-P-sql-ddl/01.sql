c844deb74f9dfdc8e0f38f87850e4a4b5de6cd41
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-06-11 00:57:56 +0100

    Added some more restrictions

--- 04-sql-ddl/01-P-sql-ddl/01.sql
+++ 04-sql-ddl/01-P-sql-ddl/01.sql
@@ -13,12 +13,12 @@ DROP TABLE IF EXISTS Duel;
 PRAGMA foreign_keys=ON;
 
 CREATE TABLE Team (
-    name        CHAR(15)    PRIMARY KEY,
-    country     CHAR(15)
+    name        CHAR(15)    NOT NULL    PRIMARY KEY,
+    country     CHAR(15)    NOT NULL
 );
 
 CREATE TABLE Aircraft (
-    model       CHAR(15)    PRIMARY KEY,
+    model       CHAR(15)    NOT NULL    PRIMARY KEY,
     horsepower  INT,
     topspeed    INT,
     width       INT,
@@ -27,22 +27,22 @@ CREATE TABLE Aircraft (
 );
 
 CREATE TABLE Pilot (
-    num         INT     PRIMARY KEY,
-    firstname   CHAR(15),
-    surname     CHAR(15),
-    nationality CHAR(15),
+    num         INT         NOT NULL    PRIMARY KEY,
+    firstname   CHAR(15)    NOT NULL,
+    surname     CHAR(15)    NOT NULL,
+    nationality CHAR(15)    NOT NULL,
     birthday    DATE,
     name        CHAR(15)    REFERENCES Team,
     model       CHAR(15)    REFERENCES Aircraft
 );
 
 CREATE TABLE Race (
-    location        CHAR(15),
-    edition         INT,
-    country         CHAR(15),
-    date            DATE,
+    location        CHAR(15)    NOT NULL,
+    edition         INT         NOT NULL,
+    country         CHAR(15)    NOT NULL,
+    date            DATE        NOT NULL    UNIQUE,
     gates           INT,
-    eliminations    INT,
+    eliminations    INT         DEFAULT 1,
     PRIMARY KEY (location, edition)
 );
 
@@ -50,28 +50,28 @@ CREATE TABLE Participation (
     num                     INT     REFERENCES Pilot,
     location                CHAR(15),
     edition                 INT,
-    trainingtime            INT,
-    trainingpos             INT,
+    trainingtime            INT     CHECK (trainingtime > 0),
+    trainingpos             INT     CHECK (trainingpos >= 1),
     trainingpenalty         INT,
     qualificationtime       INT,
-    qualificationpos        INT,
+    qualificationpos        INT     CHECK (trainingpos >= 1),
     qualificationpenalty    INT,
     eliminationtime         INT,
-    eliminationpos          INT,
+    eliminationpos          INT     CHECK (trainingpos >= 1),
     eliminationpenalty      INT,
     FOREIGN KEY (location, edition) REFERENCES Race(location, edition)
 );
 
 CREATE TABLE Duel (
-    numpilot1       INT     REFERENCES Pilot,
-    numpilot2       INT     REFERENCES Pilot,
-    location        CHAR(15),
-    edition         INT,
-    dueltype        CHAR(15),
-    timepilot1      INT,
-    timepilot2      INT,
-    penaltypilot1   INT,
-    penaltypilot2   INT,
+    numpilot1       INT         REFERENCES Pilot,
+    numpilot2       INT         REFERENCES Pilot,
+    location        CHAR(15)    NOT NULL,
+    edition         INT         NOT NULL,
+    dueltype        CHAR(15)    NOT NULL,
+    timepilot1      INT         NOT NULL    CHECK (timepilot1 > 0),
+    timepilot2      INT         NOT NULL    CHECK (timepilot2 > 0),
+    penaltypilot1   INT         NOT NULL,
+    penaltypilot2   INT         NOT NULL,
     PRIMARY KEY (raceLocation, raceEdition, pilot1, pilot2),
     FOREIGN KEY (raceLocation, raceEdition) REFERENCES Race(location, edition)
 );
